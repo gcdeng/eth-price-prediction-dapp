75567a4470978eed49d003fdd766c4e3c9f17e26
Eric Deng <eric.deng@asiayo.com>
2022-12-08 21:27:17 +0800

    modify hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,26 @@
+require("dotenv").config();
 require("@nomicfoundation/hardhat-toolbox");
+
+const ALCHEMY_KEY = process.env.ALCHEMY_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+  solidity: {
+    version: "0.8.17",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 99999,
+      },
+    },
+  },
+  networks: {
+    hardhat: {
+      forking: {
+        url: `https://eth-mainnet.g.alchemy.com/v2/${ALCHEMY_KEY}`,
+        blockNumber: 15815693,
+        enabled: true,
+      },
+    },
+  },
 };
