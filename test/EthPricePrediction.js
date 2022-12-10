f5422a4c9c2feb8ba4b8f03858160c36bcdbcca2
Eric Deng <eric.deng@asiayo.com>
2022-12-11 01:11:07 +0800

    update test

---
+++ test/EthPricePrediction.js
@@ -0,0 +1,18 @@
+const {
+  time,
+  loadFixture,
+} = require("@nomicfoundation/hardhat-network-helpers");
+const { expect } = require("chai");
+
+describe("EthPricePrediction", () => {
+  describe("Initialization", () => {});
+  describe("Oracle", () => {});
+  describe("Start round", () => {});
+  describe("Lock round", () => {});
+  describe("End round", () => {});
+  describe("Bet", () => {});
+  describe("Claim reward", () => {});
+  describe("claim treasury", () => {});
+  describe("", () => {});
+  describe("", () => {});
+});
